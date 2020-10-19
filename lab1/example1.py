494ce9b49a130837905a2a5021a3d0c9e0cb9de0
h09904564 <hasancenkyavru@hotmail.com>
2020-10-19 09:29:21 +0000

    3

--- lab1/example1.py
+++ lab1/example1.py
@@ -4,7 +4,7 @@ y = 4
 z = 0.25
 result = ((2*x + y)**2 * z**(1/2))/(x**(1/2) + y**(1/2))
 print(result)
-"""
+
 
 
 # q = 2*x**2 + 6*x - 20 = 0
@@ -15,3 +15,9 @@ c = -20
 x1 = (-b + (b**2-4*a*c)**(1/2)) / (2*a)
 x2 = (-b - (b**2-4*a*c)**(1/2)) / (2*a)
 print(x1,x2)
+"""
+
+temp = input("Celsius: ")
+temp = float(temp)
+fahrenehit = temp * 1.8 + 32
+print(fahrenehit)
