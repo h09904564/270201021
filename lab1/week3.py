1df4d6281d998debeec16e67c85eb4f3c47d4c80
h09904564 <70564139+h09904564@users.noreply.github.com>
2020-10-26 11:59:54 +0300

    Update week3.py

--- lab1/week3.py
+++ lab1/week3.py
@@ -1,3 +1,4 @@
+"""
 number = int(input("Enter a number:?"))
 
 if number > 0:
@@ -6,3 +7,16 @@ elif number < 0:
   print(number * -1)
 elif number == 0:
   print(number)
+
+
+gpa = float(input("Please enter your gpa: "))
+num_of_lectures = float(input("Please enter your number of lectures: "))
+
+if gpa >= 2.0:
+  if num_of_lectures >= 47:
+    print("Congratulations, you graduated!!")
+  else:
+    print("your number of lectures is not enough to graduate")
+else:
+  print("You don't have enough gpa")
+"""
