0c555b01fd88a7e6202bf756c8809b81f8ad7bc8
h09904564 <70564139+h09904564@users.noreply.github.com>
2020-11-09 12:30:24 +0300

    Update Week5.py

--- lab1/Week5.py
+++ lab1/Week5.py
@@ -8,5 +8,30 @@ else:
     result = number
 
 print(result)
+
+nums = [8,60,43,55,25,134,1]
+a = 0
+for i in nums:
+   a = a + i 
+print(a)
+
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+result = 1
+if b > 0:
+    for i in range(0,b,1):
+        result = a * result
+elif b == 0:
+    result = 1
+else:
+    for i in range(0,b,-1):
+        result = result / a
+print(result)
 """
 
+facto = int(input("Enter me a number: "))
+result = 1
+for i in range(1,facto+1):
+    result = i * result
+print(result)
