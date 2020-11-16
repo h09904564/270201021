cbc38b195c2c322ff53165c35d8a3fb07dbb975e
h09904564 <70564139+h09904564@users.noreply.github.com>
2020-11-16 12:06:37 +0300

    Update Week6.py

--- lab1/Week6.py
+++ lab1/Week6.py
@@ -1,1 +1,13 @@
 
+loop_times = int(input("How many number?: "))
+even = 0
+odd = 0
+for i in range(loop_times):
+    x = int(input("integer?: "))
+    if x % 2 == 0:
+        even +=1
+    else:
+        odd +=1
+result = even / (even + odd) * 100
+
+print(result)
