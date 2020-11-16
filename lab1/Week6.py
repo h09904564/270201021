7eb96c7f22992ea68524c1e9fb133fce7c506408
h09904564 <70564139+h09904564@users.noreply.github.com>
2020-11-16 12:19:29 +0300

    Update Week6.py

--- lab1/Week6.py
+++ lab1/Week6.py
@@ -1,4 +1,4 @@
-
+"""
 loop_times = int(input("How many number?: "))
 even = 0
 odd = 0
@@ -11,3 +11,14 @@ for i in range(loop_times):
 result = even / (even + odd) * 100
 
 print(result)
+"""
+x1 = (input("First number: "))      #158
+x2 = (input("Second number: "))     #15885
+a = 0
+for i in x1:
+    for n in x2:
+        if n == i:
+            a +=1
+            break
+
+print(a)
